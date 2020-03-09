10e037d089807abea82856b51dc2fcd079b1f871
Neha Mishra <neha@Nehas-MacBook-Pro.local>
2020-03-09 15:44:40 +0530

    adding the shopping collection item component

---
+++ src/components/collection-preview/collectionPreview.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './collectionPreview.style.scss';
+import CollectionItem from '../collection-item/collection-item.component';
+
+const CollectionPreview = ({ title, items }) => (
+    <div className='collection-preview'>
+        <h1 className='title'>{title}</h1>
+        <div className='preview'>
+            {items.filter((item, idx) => idx < 4)
+                .map(({ id, ...otherItemProps }) => (
+                    <CollectionItem key={id} {...otherItemProps} />
+                ))}
+        </div>
+    </div>
+)
+export default CollectionPreview;
